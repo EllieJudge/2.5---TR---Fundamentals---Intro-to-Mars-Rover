59d009d7a72bb387500cb57816f6feb7f060d70b
Ellie Judge <judge_ellie.com>
2021-06-20 21:33:47 +0100

    Adds more tests to check for collisions

--- space-exploration/mars-rover.js
+++ space-exploration/mars-rover.js
@@ -106,7 +106,6 @@ function checkForCollisions(rover, rovers) {
         }
     })
 
-
     return "All clear"
 }
 
--- space-exploration/mars-rover.test.js
+++ space-exploration/mars-rover.test.js
@@ -135,9 +135,9 @@ describe('Check for collisions', () => {
                 plateau: [5, 5]
             }
 
-            let rovers = []
+            let rovers1 = []
 
-        expect(checkForCollisions(rover1, rovers)).toEqual("All clear");
+        expect(checkForCollisions(rover1, rovers1)).toEqual("All clear");
     });
 
     it("checks rover isn't going to collide with another rover", () => {
@@ -149,7 +149,7 @@ describe('Check for collisions', () => {
             plateau: [5, 5]
         }
 
-        let rovers = [
+        let rovers2 = [
             {
                 x: 1, 
                 y: 2, 
@@ -158,7 +158,70 @@ describe('Check for collisions', () => {
             }
         ]
 
-    expect(() => checkForCollisions(rover2, rovers)).toThrow("Error: collision with another rover ahead! Abort! Abort!");
+    expect(() => checkForCollisions(rover2, rovers2)).toThrow("Error: collision with another rover ahead! Abort! Abort!");
+
+    let rover3 = {
+        x: 1, 
+        y: 2, 
+        direction: 'N', 
+        plateau: [5, 5]
+    }
+
+    let rovers3 = [
+        {
+            x: 1, 
+            y: 1, 
+            direction: 'S', 
+            plateau: [5, 5]
+        },
+        {
+            x: 3, 
+            y: 3, 
+            direction: 'W', 
+            plateau: [5, 5]
+        },
+        {
+            x: 5, 
+            y: 2, 
+            direction: 'W', 
+            plateau: [5, 5]
+        }
+    ]
+
+    expect(checkForCollisions(rover3, rovers3)).toEqual("All clear");
+
+
+    let rover4 = {
+        x: 4, 
+        y: 5, 
+        direction: 'N', 
+        plateau: [5, 5]
+    }
+
+    let rovers4 = [
+        {
+            x: 1, 
+            y: 1, 
+            direction: 'S', 
+            plateau: [5, 5]
+        },
+        {
+            x: 3, 
+            y: 3, 
+            direction: 'W', 
+            plateau: [5, 5]
+        },
+        {
+            x: 4, 
+            y: 5, 
+            direction: 'W', 
+            plateau: [5, 5]
+        }
+    ]
+
+    expect(() => checkForCollisions(rover4, rovers4)).toThrow("Error: collision with another rover ahead! Abort! Abort!");
+
+
 });
 })
 
