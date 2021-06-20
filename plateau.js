ffc5b768b6374190aba85b56c0624b53ed5f9b90
Ellie Judge <judge_ellie.com>
2021-06-20 17:40:51 +0100

    Refactored checkPlateau and tests

--- plateau.js
+++ plateau.js
@@ -1,26 +1,23 @@
-
 function checkPlateau(plateau) {
 
     let platArray = plateau.split(' ')
-
 
     let x = Number.parseInt(platArray[0])
     let y = Number.parseInt(platArray[1])
 
 
-    if (x === NaN || y === NaN) {
+    if (isNaN(x) || isNaN(y)) {
         throw new Error("Error: x and y co-ordinates must be numbers");
     }
-    if (x === 0 || y === 0) {
-        throw new Error("Error: x and y co-ordinates must be greater than 0");
-    }
-    if (x < 0 || y < 0) {
-        throw new Error("Error: x and y co-ordinates must be greater than 0");
-    }
 
+    if (x <= 0 || y <= 0) {
+        throw new Error("Error: x and y co-ordinates must be greater than 0");
+    }
 
     return [x, y]
 }
+
+
 
 
 module.exports = {
--- plateau.test.js
+++ plateau.test.js
@@ -11,21 +11,17 @@ describe('Check is a valid plateau', () => {
     // let minSize = 0; 
     // let maxSize = 5; 
 
-    // test('Check co-ordinates are not 0', () => {
-    //     expect(() => checkPlateau(0, 0)).toThrow("Error: x and y co-ordinates must be greater than 0");
-    //     expect(() => checkPlateau(0, 5)).toThrow("Error: x and y co-ordinates must be greater than 0");
-    // });
-    // test('Check co-ordinates are not less than 0', () => {
-    //     expect(() => checkPlateau(-1, -5)).toThrow("Error: x and y co-ordinates must be greater than 0");
-    //     expect(() => checkPlateau(-1, -0)).toThrow("Error: x and y co-ordinates must be greater than 0");
-    // });
-    // test('Check co-ordinates are integers', () => {
-    //     expect(() => checkPlateau(0.394, 3.91)).toThrow("Error: x and y co-ordinates must be integers");
-    // });
-    // test('Check co-ordinates are numbers', () => {
-    //     expect(() => checkPlateau("donkey", "melon")).toThrow("Error: x and y co-ordinates must be numbers");
-    //     expect(() => checkPlateau("0", "5")).toThrow("Error: x and y co-ordinates must be numbers");
-    // });
+    test('Check co-ordinates are not 0', () => {
+        expect(() => checkPlateau('0 0')).toThrow("Error: x and y co-ordinates must be greater than 0");
+        expect(() => checkPlateau('0 5')).toThrow("Error: x and y co-ordinates must be greater than 0");
+    });
+    test('Check co-ordinates are not less than 0', () => {
+        expect(() => checkPlateau('-1 -5')).toThrow("Error: x and y co-ordinates must be greater than 0");
+        expect(() => checkPlateau('-1 -10')).toThrow("Error: x and y co-ordinates must be greater than 0");
+    });
+    test('Check co-ordinates are numbers', () => {
+        expect(() => checkPlateau("donkey melon")).toThrow("Error: x and y co-ordinates must be numbers");
+    });
     const validPlateau = '5 5';
     it('returns co-ordinates if valid plateau co-ordinates have been received', () => {
         expect(checkPlateau(validPlateau)).toEqual([5, 5]);
