4a32f0c44c1d398edc02d4ffa8105c01429b80cb
Ellie Judge <judge_ellie.com>
2021-06-20 21:28:01 +0100

    Passes test it checks for collisions with other rovers

--- space-exploration/mars-rover.js
+++ space-exploration/mars-rover.js
@@ -99,6 +99,14 @@ function changeDirection(currentDirection, turn) {
 }
 
 function checkForCollisions(rover, rovers) {
+
+    rovers.forEach(existingRover => {
+        if (existingRover.x === rover.x && existingRover.y === rover.y) {
+            throw new Error("Error: collision with another rover ahead! Abort! Abort!")
+        }
+    })
+
+
     return "All clear"
 }
 
