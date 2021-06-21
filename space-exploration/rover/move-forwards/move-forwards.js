6d5dc3e7a0452f92edec929ae011388565daef91
Ellie Judge <judge_ellie.com>
2021-06-21 17:57:41 +0100

    Passes test should return y + 1 if position is N

--- space-exploration/rover/move-forwards/move-forwards.js
+++ space-exploration/rover/move-forwards/move-forwards.js
@@ -1,22 +1,19 @@
 
 function moveForwards(rover) {
 
-            if (rover.direction === "W") 
-                rover.x -= 1
-            
+    if (rover.direction === "W")
+        rover.x -= 1
 
-            
-            if (rover.direction === "E") 
-                rover.x += 1
-            
-                
-                if (rover.direction === "S")
-                rover.y -= 1
-                
-                return rover
-            // if (rover.direction === "N")
-            // rover.y += 1
-    
+    if (rover.direction === "E")
+        rover.x += 1
+
+    if (rover.direction === "S")
+        rover.y -= 1
+
+    if (rover.direction === "N")
+        rover.y += 1
+
+    return rover
 }
 
 module.exports = moveForwards;
