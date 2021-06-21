ddfe9406f6cc8b51446b5d8e596966a19a1cc1e3
Ellie Judge <judge_ellie.com>
2021-06-21 17:52:59 +0100

    Passes test should return rover X coordinate plus one if direction is E

--- space-exploration/rover/move-forwards/move-forwards.js
+++ space-exploration/rover/move-forwards/move-forwards.js
@@ -6,11 +6,11 @@ function moveForwards(rover) {
             
 
             
-            // if (rover.direction === "E") {
-            //     rover.x += 1
-            // }
+            if (rover.direction === "E") 
+                rover.x += 1
             
             return rover
+            
             // if (rover.direction === "S")
             // rover.y -= 1
 
