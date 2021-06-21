783cec477f9af4563dbae51f65353a19e76c4532
Ellie Judge <judge_ellie.com>
2021-06-21 13:33:30 +0100

    Moves checkMoveIsSafe and tests to separate folder

--- space-exploration/modules.js
+++ space-exploration/modules.js
@@ -1,6 +1,4 @@
 module.exports = {
-    // checkInitialPosition : require('./checks/check-initial-position/check-initial-position.js'),
-
     checkInitialPosition : require('./checks/check-initial-position/check-initial-position'),
-    checkMoveIsSafe : require('./checks/check-move-is-safe')
+    checkMoveIsSafe : require('./checks/check-move-is-safe/check-move-is-safe')
  }
--- space-exploration/plateau.test.js
+++ space-exploration/plateau.test.js
@@ -2,12 +2,6 @@ const {
     createPlateau
 } = require("./plateau");
 
-const {
-    
-    checkMoveIsSafe
-} = require("./modules")
-
-
 
 describe('Check is a valid plateau', () => {
 
@@ -28,38 +22,3 @@ describe('Check is a valid plateau', () => {
     });
 });
 
-describe('Check move is safe', () => {
-
-    const rover = {
-        x: 0,
-        y: 0, 
-        direction: 'W', 
-        plateau: [5, 5]
-    }
-
-    test('If move sends rover off plateau edge, throw error', () => {
-        expect(() => checkMoveIsSafe(rover)).toThrow("Error: rover is about to fall off left edge of plateau");
-    });
-
-    const rover2 = {
-        x: 5,
-        y: 5, 
-        direction: 'N', 
-        plateau: [5, 5]
-    }
-
-    test('If move sends rover off plateau edge, throw error', () => {
-        expect(() => checkMoveIsSafe(rover2)).toThrow("Error: rover is about to fall off right edge of plateau");
-    });
-
-    const rover3 = {
-        x: 2,
-        y: 3, 
-        direction: 'W', 
-        plateau: [5, 5]
-    }
-
-    test('If move is safe, return "Safe"', () => {
-        expect(checkMoveIsSafe(rover3)).toBe("Safe");
-    });
-});
