25a8c7e8774f9a8fb04f8f04b982f9dc07dccacd
Ellie Judge <judge_ellie.com>
2021-06-25 12:45:54 +0100

    Deleted tests tthat werent really doing anything

--- space-exploration/rover/get-final-position/get-final-position.test.js
+++ space-exploration/rover/get-final-position/get-final-position.test.js
@@ -1,23 +1,5 @@
 const { getFinalPosition } = require('./get-final-position');
 
-//Not happy delete if necessary
-describe('moveForwards function is called from getFinalPosition', () => {
-
-    // this seems wrong ?? 
-    move = require('./get-final-position').getFinalPosition;
-    move = jest.fn();
-
-    it('should be called with plateau co-ordinates, rovers initial position and instructions, as arguments', () => {
-
-        const plateau = [5, 5]
-        const initialPos = [1, 2, 'N']
-        const instructions = 'LMLMLMLMM'
-
-        move(plateau, initialPos, instructions)
-        expect(move).toHaveBeenCalledTimes(1); // true
-        expect(move).toHaveBeenCalledWith(plateau, initialPos, instructions);
-    });
-});
 
 describe('getFinalPosition returns a rovers final position', () => {
 
